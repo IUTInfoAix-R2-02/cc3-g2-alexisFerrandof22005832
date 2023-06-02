5ae47ba49272c62a1fc326192a7264a1c25e527a
alexisFerrandof22005832 <123471555+alexisFerrandof22005832@users.noreply.github.com>
2023-06-02 16:22:59 +0200

    Update ToileController.java

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,27 +1,12 @@
 package fr.amu.iut.cc3;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ToileController implements Initializable {
@@ -31,10 +16,72 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    TextField T1;
+
+    @FXML
+    TextField T2;
+
+    @FXML
+    TextField T3;
+
+    @FXML
+    TextField T4;
+
+    @FXML
+    TextField T5;
+
+    @FXML
+    TextField T6;
+
+    @FXML
+    Label Error;
+
+    @FXML
+    Button Tracer = new Button();
+
+    @FXML
+    Button Vider = new Button();
+
+    String Competence1 = T1.getText();
+    String Competence2 = T2.getText();
+    String Competence3 = T3.getText();
+    String Competence4 = T4.getText();
+    String Competence5 = T5.getText();
+    String Competence6 = T6.getText();
+
+    String MsgError = Error.getText();
+
+
+    int Note1 = Integer.parseInt(Competence1);
+    int Note2 = Integer.parseInt(Competence2);
+    int Note3 = Integer.parseInt(Competence3);
+    int Note4 = Integer.parseInt(Competence4);
+    int Note5 = Integer.parseInt(Competence5);
+    int Note6 = Integer.parseInt(Competence6);
+
+    @FXML
+    private void TacerCliked(){
+        getXRadarChart(0,6);
+        getYRadarChart(0,6);
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        getXRadarChart(Note1,1);
+        getXRadarChart(Note1,1);
+        getXRadarChart(Note2,2);
+        getXRadarChart(Note2,2);
+        getXRadarChart(Note3,3);
+        getXRadarChart(Note3,3);
+        getXRadarChart(Note4,4);
+        getXRadarChart(Note4,4);
+        getXRadarChart(Note5,5);
+        getXRadarChart(Note5,5);
+        getXRadarChart(Note6,6);
+        getXRadarChart(Note6,6);
     }
+
 
     int getXRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur + Math.cos(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
@@ -45,5 +92,9 @@ public class ToileController implements Initializable {
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
+    
+
+
+
 
 }
